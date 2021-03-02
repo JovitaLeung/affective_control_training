482403cdc4cfbb192a550c903b6e3cd1fb997359
Jovita Leung <jovita.leung@ucl.ac.uk>
2021-03-02 11:15:06 +0000

    updated t2 scoring script

--- questionnaire_preprocessing.R
+++ questionnaire_preprocessing.R
@@ -16,6 +16,11 @@ t1=read.csv("/Users/jovitaleung/Dropbox/PhD/Affective Control Training Study/ac_
 colnames(t1)[colnames(t1) == 'subjid'] <- 'subjno'
 summary(t1)
 
+t2=read.csv("/Users/jovitaleung/Dropbox/PhD/Affective Control Training Study/ac_training/data/t2_questionnaire_raw.csv")
+colnames(t2)[colnames(t2)=="id"] <- "subjno"
+t2$subjno=as.factor(t2$subjno)
+summary(t2)
+
 d=read.csv("/Users/jovitaleung/Dropbox/PhD/Affective Control Training Study/ac_training/data/baseline_mastersheet.csv")
 d1=subset(d,select = c("id","gender","age","ses","pre.post.days","iq.sum","pds.sum"))
 names(d1)=c("subjno","gender","decimal_age","ses","test_days","iq_sum","pds")
@@ -35,11 +40,10 @@ summary(t1)
 # summary(age$decimal_age)
 # demos=subset(age,select = c("subjno","gender","age","decimal_age"))
 
-## Scoring questionnaire data --- 
-# T1 
+## Scoring T1 questionnaire data --- 
 #SDQ
 sdq=subset(t1[,c(1,22:46)])
-sdq[,2:26][sdq[,2:26]=="1"] <- 11 #recode values to right value for scoring
+sdq[,2:26][sdq[,2:26]=="1"] <- 11 #T1 questionnaire scored answers incorrectly. So recode values to right value for scoring
 sdq[,2:26][sdq[,2:26]=="2"] <- 22
 sdq[,2:26][sdq[,2:26]=="3"] <- 33
 sdq[,2:26][sdq[,2:26]=="11"] <- 0
@@ -59,7 +63,6 @@ sdq1=subset(sdq,select = c('subjno','sdq_sum_t1','sdq_emotional_t1','sdq_conduct
 sdq1[sdq1==0]=NA #remove zeros created with missing data
 summary(sdq1)
 
-
 #DERS
 ders=subset(t1[,c(1,47:82)])
 summary(ders)
@@ -77,7 +80,6 @@ ders1=subset(ders, select = c('subjno','ders_sum_t1','ders_nonaccept_t1','ders_g
 ders1[ders1==0]=NA
 summary(ders1)
 
-
 #PANAS
 panas=subset(t1[,c(1:21)])
 panas$panas_pos_t1<- rowSums(panas[c(2,4,6,10,11,13,15,17,18,20)], na.rm = TRUE)
@@ -90,7 +92,6 @@ scs[,c(2,4,6,8,9,10,11,12,13)]= 5-scs[,c(2,4,6,8,9,10,11,12,13)] #reverse scorin
 scs$scs_t1 <- rowSums(scs[2:14], na.rm = TRUE)
 scs1=subset(scs,select = c('subjno','scs_t1'))
 
-
 ## Mergeing T1 questionnaire data --- 
 t1_scored=Reduce(function(x,y) merge(x=x,y=y, by="subjno"), 
                  list(demos, sdq1, ders1, panas1, scs1))
@@ -98,3 +99,69 @@ length(unique(t1_scored$subjno)) #231
 t1_scored=t1_scored[!duplicated(t1_scored$subjno),]
 summary(t1_scored)
 write.csv(t1_scored,file="/Users/jovitaleung/Dropbox/PhD/Affective Control Training Study/ac_training/processed_data/t1_questionnaire_scored.csv")
+
+
+
+########################################
+## Scoring T2 questionnaire data ---
+#SDQ
+sdq=subset(t2[,c(1,22:46)])
+# sdq[,2:26][sdq[,2:26]=="1"] <- 11 #T2 questionnaire was scored correctly so do not run this 
+# sdq[,2:26][sdq[,2:26]=="2"] <- 22
+# sdq[,2:26][sdq[,2:26]=="3"] <- 33
+# sdq[,2:26][sdq[,2:26]=="11"] <- 0
+# sdq[,2:26][sdq[,2:26]=="22"] <- 1
+# sdq[,2:26][sdq[,2:26]=="33"] <- 2
+
+sdq[,c(8,12,15,22,26)]= 2-sdq[,c(8,12,15,22,26)]#reverse scoring 
+
+sdq$sdq_sum_t2<- rowSums(sdq[c(-1,-2,-5,-10,-18,-21)], na.rm=TRUE) #total difficulties score (sum of all items except prosocial scale)
+sdq$sdq_emotional_t2 <- rowSums(sdq[c(4,9,14,17,25)], na.rm=TRUE) #emotional problem subscale
+sdq$sdq_conduct_t2 <- rowSums(sdq[c(6,8,13,19,23)], na.rm=TRUE) #conduct problem subscale
+sdq$sdq_hyperactivity_t2<- rowSums(sdq[c(3,11,16,22,26)], na.rm=TRUE) #hyperactivity subscale
+sdq$sdq_peer_t2<- rowSums(sdq[c(7,12,15,20,24)], na.rm=TRUE) #peer problem subscale
+sdq$sdq_prosocial_t2 <- rowSums(sdq[c(2,5,10,18,21)], na.rm=TRUE) #prosocial subscale
+
+sdq1=subset(sdq,select = c('subjno','sdq_sum_t2','sdq_emotional_t2','sdq_conduct_t2','sdq_hyperactivity_t2','sdq_peer_t2','sdq_prosocial_t2'))
+sdq1[sdq1==0]=NA #remove zeros created with missing data
+summary(sdq1)
+
+
+#DERS
+ders=subset(t2[,c(1,55:90)])
+summary(ders)
+ders[,c(2,3,7,8,9,11,18,21,23,25,35)]= 5-ders[,c(2,3,7,8,9,11,18,21,23,25,35)] #reverse scoring
+
+ders$ders_sum_t2 <- rowSums(ders[2:37], na.rm=TRUE) #overall score
+ders$ders_nonaccept_t2<- rowSums(ders[c(26,22,13,12,30,24)], na.rm=TRUE) #nonacceptance of emotional responses subscale
+ders$ders_goals_t2 <- rowSums(ders[c(27,19,14,34,21)], na.rm=TRUE) #difficulties engaging in goal-directed subsclae
+ders$ders_impulse_t2 <- rowSums(ders[c(33,28,15,20,4,25)], na.rm=TRUE) #impulse control difficulties 
+ders$ders_aware_t2 <- rowSums(ders[c(7,3,11,18,9,35)], na.rm=TRUE) #lack of emotional awareness
+ders$ders_strategies_t2 <- rowSums(ders[c(17,16,32,36,29,23,37,31)], na.rm=TRUE) #limited access to emotion regulation strategies
+ders$ders_clarity_t2 <- rowSums(ders[c(6,5,10,8,2)], na.rm=TRUE) #lack of emotional clarity
+
+ders1=subset(ders, select = c('subjno','ders_sum_t2','ders_nonaccept_t2','ders_goals_t2','ders_impulse_t2','ders_aware_t2','ders_strategies_t2','ders_clarity_t2'))
+# ders1[ders1==0]=NA
+summary(ders1)
+
+#PANAS
+panas=subset(t2[,c(1:21)])
+panas$panas_pos_t2<- rowSums(panas[c(2,4,6,10,11,13,15,17,18,20)], na.rm = TRUE)
+panas$panas_neg_t2<- rowSums(panas[c(3,5,7,8,9,12,14,16,19,21)], na.rm = TRUE)
+panas1=subset(panas, select = c('subjno','panas_pos_t2','panas_neg_t2'))
+summary(panas1)
+
+#Self-control Scale
+scs=subset(t2[,c(1,91:103)])
+scs[,c(2,4,6,8,9,10,11,12,13)]= 5-scs[,c(2,4,6,8,9,10,11,12,13)] #reverse scoring
+scs$scs_t2 <- rowSums(scs[2:14], na.rm = TRUE)
+scs1=subset(scs,select = c('subjno','scs_t2'))
+summary(scs1)
+
+## Mergeing T2 questionnaire data --- 
+t2_scored=Reduce(function(x,y) merge(x=x,y=y, by="subjno"), 
+                 list(demos, sdq1, ders1, panas1, scs1))
+length(unique(t2_scored$subjno)) #177
+t2_scored=t2_scored[!duplicated(t2_scored$subjno),]
+summary(t2_scored)
+write.csv(t2_scored,file="/Users/jovitaleung/Dropbox/PhD/Affective Control Training Study/ac_training/processed_data/t2_questionnaire_scored.csv")
